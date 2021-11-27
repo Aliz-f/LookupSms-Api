35c9fbb0a3b3da4737bea342ae4c4c30ef7161fd
Aliz-f <fad127alireza2@gmail.com>
2021-11-27 14:22:54 +0330

    init commit -> remove apps and add new app

--- sms_lookup/apps.py
+++ sms_lookup/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LookupConfig(AppConfig):
+class SmsLookupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lookup'
+    name = 'sms_lookup'
